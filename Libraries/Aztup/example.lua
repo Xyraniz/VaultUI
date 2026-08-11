e5088a26b0992dfae6ee4f312bdf084476d9a951
Xyraniz <109302452+Xyraniz@users.noreply.github.com>
2026-08-11 02:37:08 -0600

    Update library source URL in example.lua

--- Libraries/Aztup/example.lua
+++ Libraries/Aztup/example.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/ui-libraries/main/aztup/src.lua"))();
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xyraniz/VaultUI/refs/heads/main/Libraries/Aztup/source.lua"))();
 
 
 local Gamer = library:CreateSection("A");
