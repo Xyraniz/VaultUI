642a4d42b47c420151612e57e3edfc456cfc15ed
Synergy Team <SynergyHubOfficial@protonmail.com>
2026-08-27 17:29:39 -0600

    Add new showcases to showcaseTable
    Added new showcase entries with embed URLs.

--- web/showcases.js
+++ web/showcases.js
@@ -1,15 +1,27 @@
 const showcaseTable = Object.freeze({
+  '0x37': {
+    embedUrl: 'https://files.catbox.moe/5buknf.mp4'
+  },
+  apple: {
+    embedUrl: 'https://files.catbox.moe/5gwc7y.mp4'
+  },
   armenta_lib: {
     embedUrl: 'https://files.catbox.moe/zl4njb.mp4'
   },
   aztup: {
     embedUrl: 'https://files.catbox.moe/di65fq.mp4'
   },
+  avilon_modified: {
+    embedUrl: 'https://files.catbox.moe/duv7ak.mp4'
+  },
   bacon: {
     embedUrl: 'https://files.catbox.moe/bpo9er.mp4'
   },
   discord_lib: {
     embedUrl: 'https://files.catbox.moe/wq7znk.mp4'
+  },
+  flux: {
+    embedUrl: 'https://files.catbox.moe/1mg3kd.mp4'
   },
   synergyui: {
     embedUrl: 'https://files.catbox.moe/cw0lli.mp4'
